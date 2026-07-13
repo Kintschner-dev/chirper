761cc99973fa1125c226c58f6e59fd73426d1133
Rafael <kintschner.dev@gmail.com>
2026-07-13 18:25:15 -0300

    What is MVC?

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ChirpeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [ChirpeController::class, 'index']);
+

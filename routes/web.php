fcde3be0d296dacde1cbbb0c987e7ef7541ebc34
Rafael <kintschner.dev@gmail.com>
2026-07-13 18:37:35 -0300

    Working with the database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\ChirpeController;
+use App\Http\Controllers\ChirperController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ChirpeController::class, 'index']);
+Route::get('/', [ChirperController::class, 'index']);
 
